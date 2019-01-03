936b5b2010e621e669765add0cfee2bc3f1b1f54
github username <github email address>
2019-01-03 14:49:42 -0600

    Changed shit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,15 @@
 class PicturesController < ApplicationController
+    def index
+        
+    end
+
+    def create
+        @picture = Picture.create(picture_params)
+    end
+
+    private
+
+    def picture_params
+        params.require(:picture).permit(:image_url, :title, :user_id)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to user_path(@user)
+        else
+            flash[:error] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
+
 end
