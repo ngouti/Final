48c30d7e47f953e415b2bf90920949799ccab4d6
github username <github email address>
2019-01-03 19:46:54 -0600

    Fixed login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/signup", to: "users#new", as: "signup"
   get "/login", to: "sessions#new"
-  post "/sessions", to: "sessions#create"
+  post "/login", to: "sessions#create"
   delete "/sessions", to: "sessions#destroy"
-  post '/comments', to: 'comments#create'
+  # post '/comments', to: 'comments#create'
 end
